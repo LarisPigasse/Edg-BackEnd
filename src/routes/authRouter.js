73f24f2acf42e363bcd2b40078c630aa919e28a5
Gabriel <gabrielpantoli@gmail.com>
2023-06-01 21:44:20 +0200

    fix profilo login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,13 +3,15 @@ import { Router } from "express";
 import {
     login,
     endpointCrypta,
-    generaToken
+    generaToken,
+    profilo
 } from "../controllers/utentiController.js";
 
 const router = Router();
 
 router.get("/genera-token", generaToken);
 router.post("/login", login);
+router.get("/profilo", profilo);
 router.post("/endpoint-crypta", endpointCrypta);
 
 export default router
